d66eddcafc8dc3c5fc80513c668ba0c10c3d53ad
rravenorr <148618881+rravenorr@users.noreply.github.com>
2025-02-07 20:37:14 +0800

    1

--- core/migrations/0016_remove_employee_departments_employee_department_and_more.py
+++ core/migrations/0016_remove_employee_departments_employee_department_and_more.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0014_remove_employee_department_and_more'),
+        ('core', '0015_remove_employee_department_and_more'),
     ]
 
     operations = [
